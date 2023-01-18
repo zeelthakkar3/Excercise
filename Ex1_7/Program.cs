42cd36abf027df42c57c1d2f9f6aa2120ec4a62a
zeelthakkar3 <122888742+zeelthakkar3@users.noreply.github.com>
2023-01-18 12:38:57 +0530

    -- initial Commit

---
+++ Ex1_7/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace MyApplication
+{
+    class Member
+    {
+        public string name = "";
+        public int age;
+        public long phonenumber;
+        public string address = "";
+        public double salary;
+        public void printSalary()
+        {
+            Console.WriteLine(salary);
+        }
+
+    }
+    class Employee : Member
+    {
+        public string specialization  ="";
+
+    }
+
+    class Manager : Member
+    {
+        public string department = "";
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee();
+            emp.name = "Jenny";
+            emp.age = 25;
+            emp.phonenumber = 1234567890;
+            emp.address = "India";
+            emp.salary = 20000;
+            emp.specialization = "dotnet";
+            Console.WriteLine(emp.name);
+            Console.WriteLine(emp.age);
+            Console.WriteLine(emp.phonenumber);
+            Console.WriteLine(emp.address);
+            emp.printSalary();
+            Console.WriteLine(emp.specialization);
+
+            Console.WriteLine();
+
+            Manager mng = new Manager();
+            mng.name = "Smith";
+            mng.age = 22;
+            mng.phonenumber = 9988776612;
+            mng.address = "U.S.A";
+            mng.salary = 100000;
+            mng.department = "Product Devlopment";
+            Console.WriteLine(mng.name);
+            Console.WriteLine(mng.age);
+            Console.WriteLine(mng.phonenumber);
+            Console.WriteLine(mng.address);
+            Console.WriteLine(mng.salary);
+            Console.WriteLine(mng.department);
+            
+           
+        }
+    }
+
+}
---
+++ Ex1_8/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace MyApplication
+{
+    class Rectangle
+    {
+        public int l, b;
+
+        public Rectangle(int l, int b)
+        {
+            this.l = l;
+            this.b = b;
+        }
+        public void rectangleArea()
+        {
+            Console.WriteLine("Area of a rectangle : " + (l * b));
+        }
+
+        public void rectanglePerimeter()
+        {
+            Console.WriteLine("Perimeter of a rectangle : " + 2 * (l + b));
+            
+        }
+
+        
+    }
+
+    class Square : Rectangle
+    {
+        int s;
+        public Square(int l, int b) : base(l, b)
+        {
+            this.s = l;
+            squareArea();
+            squarePerimeter();
+
+        }
+
+        public void squareArea()
+        {
+            Console.WriteLine("Area of a Square : " + Math.Pow(s, 2));
+        }
+
+        public void squarePerimeter()
+        {
+            Console.WriteLine("Perimeter of a Square : " + 2 * s);
+            Console.WriteLine();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Square sq = new(4, 6);
+            sq.rectangleArea();
+            sq.rectanglePerimeter();
+        }
+    }
+
+}
